ae969ec09f9dea9722209ae48866b3dea78a2046
divy6976 <divyprakashpandey6@gmail.com>
2025-08-28 22:20:21 +0530

    Api route

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({extended:true}));  //form data url se lene ko liye
 
 
 app.post("/signup",async (req,res)=>{
+    //added the data to the database
 
     const {firstName,lastName,email,password,age}=req.body;
     console.log(req.body);
@@ -37,7 +38,87 @@ if(!firstName || !lastName || !email || !password || !age){
              res.status(500).send("Internal Server Error");
          }
 
+          
 })
+
+
+//get the user from email
+app.get("/user",async(req,res)=>{
+   const email = req.body.email;
+       if(!email){
+        return res.status(400).send("Email is required");
+       }
+       try {
+         const user= await User.find({email})
+
+      if(user.length === 0){
+         return res.status(400).send("User not found");
+
+        
+      }
+      console.log(user);
+      console.log("user mil gya")
+      return  res.status(200).json(user)
+        
+       } catch (error) {
+          console.log(error);
+          res.status(500).send("Something went wrong");
+       }
+     
+
+})
+
+
+app.get("/feed",async (req,res)=>{
+    //get all user profiles from database
+    try {
+           const users = await User.find({})
+           res.status(200).json(users)
+    } catch (error) {
+           console.log(error);
+           res.status(500).send("profiles not found");
+    }
+    
+})
+
+//get user by id
+
+app.get("/profile",async(req,res)=>{
+    const id=req.body.id;
+    try {
+
+          const user= await User.findById({_id:id})
+
+  if(!user){
+      return res.status(404).send("User not found");
+  }
+
+  return res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("User profile by id not found");
+    }
+
+
+})
+
+app.delete("/delete",async(req,res)=>{
+    const id=req.body.id;
+    try {
+        const user= await User.findByIdAndDelete({_id:id})
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+        console.log(user)
+        res.send("User deleted successfully");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+
 
 
 
